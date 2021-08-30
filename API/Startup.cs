91b60a63f1937b98bc275d662bb8060b45012c0c
CuongVM <sycuongvm2@gmail.com>
2021-08-30 16:34:40 +0700

    End of section 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using Infrastructure.Data;
 using API.Helpers;
 using API.Middleware;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -24,6 +25,10 @@ namespace API
       services.AddAutoMapper(typeof(MappingProfiles));
       services.AddControllers();
       services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("Default")));
+      services.AddSingleton<IConnectionMultiplexer>(_ => {
+        var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+        return ConnectionMultiplexer.Connect(configuration);
+      });
 
       services.AddApplicationServices();
       services.AddSwaggerDocumentation();
