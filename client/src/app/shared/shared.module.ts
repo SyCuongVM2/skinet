91c86a4e4d7bc8ddb7ce143174ef2260fca11108
CuongVM <sycuongvm2@gmail.com>
2021-09-06 19:18:50 +0700

    End of section 19

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -8,16 +8,22 @@ import { OrderTotalsComponent } from './components/order-totals/order-totals.com
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { StepperComponent } from './components/stepper/stepper.component';
+import { BasketSummaryComponent } from './components/basket-summary/basket-summary.component';
 
 @NgModule({
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
     OrderTotalsComponent,
-    TextInputComponent
+    TextInputComponent,
+    StepperComponent,
+    BasketSummaryComponent
   ],
   imports: [
     CommonModule,
+    CdkStepperModule,
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
     BsDropdownModule.forRoot(),
@@ -32,6 +38,9 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     ReactiveFormsModule,
     TextInputComponent,
     BsDropdownModule,
+    CdkStepperModule,
+    StepperComponent,
+    BasketSummaryComponent
   ]
 })
 export class SharedModule { }
